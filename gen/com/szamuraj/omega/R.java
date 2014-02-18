b5b5eb4efeca42619a6f6056f1ab8583e835a8c4
S4mur4i <admin@szamuraj.com>
2014-02-18 21:40:12 +0100

    Added Location Listener

--- gen/com/szamuraj/omega/R.java
+++ gen/com/szamuraj/omega/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int allbtn=0x7f070001;
+        public static final int clearbtn=0x7f070003;
         public static final int locbtn=0x7f070000;
         public static final int nearbtn=0x7f070002;
-        public static final int nobtn=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/szamuraj/omega/MainActivity.java
+++ src/com/szamuraj/omega/MainActivity.java
@@ -3,14 +3,25 @@ package com.szamuraj.omega;
 import java.io.IOException;
 import java.util.List;
 
-
 import android.app.ListActivity;
-import android.database.SQLException;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity{
-	
+	private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1; // in Meters
+    private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000; // in Milliseconds
+    
+    protected LocationManager locationManager;
+    
+    protected Button retrieveLocationButton;
 	
 	  @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,23 @@ public class MainActivity extends ListActivity{
 		 setListAdapter(adapter);
 			
 			myDbHelper.close();
+			
+			retrieveLocationButton = (Button) findViewById(R.id.locbtn);
+	        
+	        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	        
+	        locationManager.requestLocationUpdates(
+	                LocationManager.GPS_PROVIDER, 
+	                MINIMUM_TIME_BETWEEN_UPDATES, 
+	                MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
+	                new MyLocationListener()
+	        );
+			retrieveLocationButton.setOnClickListener(new OnClickListener() {
+		         @Override
+		         public void onClick(View v) {
+		             showCurrentLocation();
+		         }
+		 }); 
 	}
 
 	@Override
@@ -42,5 +70,62 @@ public class MainActivity extends ListActivity{
 		
 	}
 
+	public void clearevents(View view) {
+		 ArrayAdapter adapter = (ArrayAdapter) getListAdapter();
+		 adapter.clear();
+	}
+	public void allevents(View view) {
+		 ArrayAdapter adapter = (ArrayAdapter) getListAdapter();
+		 ControllerEvent eventoperation = new ControllerEvent(this);
+		 List values = eventoperation.getAllEvents();
+		 for (  int i = 0; i < values.size(); i++ ) {
+			 adapter.add(values.get(i));
+		 }
+		 setListAdapter(adapter);
+	}
 
+	           
+
+ protected void showCurrentLocation() {
+
+     Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+     if (location != null) {
+         String message = String.format(
+                 "Current Location \n Longitude: %1$s \n Latitude: %2$s",
+                 location.getLongitude(), location.getLatitude()
+         );
+         Toast.makeText(MainActivity.this, message,
+                 Toast.LENGTH_LONG).show();
+     }
+
+ }
+ private class MyLocationListener implements LocationListener {
+
+     public void onLocationChanged(Location location) {
+         String message = String.format(
+                 "New Location \n Longitude: %1$s \n Latitude: %2$s",
+                 location.getLongitude(), location.getLatitude()
+         );
+         Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+     }
+
+     public void onStatusChanged(String s, int i, Bundle b) {
+         Toast.makeText(MainActivity.this, "Provider status changed",
+                 Toast.LENGTH_LONG).show();
+     }
+
+     public void onProviderDisabled(String s) {
+         Toast.makeText(MainActivity.this,
+                 "Provider disabled by the user. GPS turned off",
+                 Toast.LENGTH_LONG).show();
+     }
+
+     public void onProviderEnabled(String s) {
+         Toast.makeText(MainActivity.this,
+                 "Provider enabled by the user. GPS turned on",
+                 Toast.LENGTH_LONG).show();
+     }
+
+ }
 }
